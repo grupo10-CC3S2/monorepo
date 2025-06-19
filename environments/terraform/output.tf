f218518b92a78b8992496bc39cf3b57fc9ac2786
Alex Vega <axvg@users.noreply.github.com>
2025-06-19 02:12:37 -0500

    feat(tf): muestre informacion de instancias creadas de network_module

--- environments/terraform/output.tf
+++ environments/terraform/output.tf
@@ -2,5 +2,6 @@ output "info_completa" {
   description = "Info completa de la infraestructura"
   value = {
     network_info = module.network.network_info
+    compute_info = module.compute.compute_info
   }
 }
