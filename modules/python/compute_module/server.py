679d3a3c9be2d74f4bb50d725d9091828be498c9
Alex Vega <axvg@users.noreply.github.com>
2025-06-18 19:51:30 -0500

    fix(py): fix dependencia de computer_module

--- modules/python/compute_module/server.py
+++ modules/python/compute_module/server.py
@@ -4,6 +4,7 @@ class ServerFactoryModule:
         self.network = network
         self.server_type = "web"
         self.instance_count = instance_count
+        self._depends_on = [self.network]
 
     def build(self):
         command = f"echo 'Creando server {self.name} con network {self.network.name}'"
@@ -14,6 +15,7 @@ class ServerFactoryModule:
                     self.name: {
                         "count": self.instance_count,
                         "provisioner": {"local-exec": {"command": command}},
+                        "depends_on": [f"null_resource.{dep.name}" for dep in self._depends_on],
                     },
                 }
             }
