0652494f07979bb70c75ab0beea4a9d73ba62ae7
Alex Vega <axvg@users.noreply.github.com>
2025-06-19 02:11:51 -0500

    feat(tf): agrega output para mostrar info resumido de las instacias creadas

--- modules/terraform/compute_module/outputs.tf
+++ modules/terraform/compute_module/outputs.tf
@@ -2,3 +2,17 @@ output "server_name_out" {
   description = "El nombre del server creado"
   value       = var.name
 }
+
+output "compute_info" {
+  description = "Informacion de servidores creado"
+  value = [
+    for i in range(var.instance_count) : {
+      name         = "${var.name}-${i + 1}"
+      region       = var.region
+      network_name = var.network_name
+      index_count  = i + 1
+      version      = "v0.4.0"
+    }
+  ]
+  depends_on = [var.network_name]
+}
