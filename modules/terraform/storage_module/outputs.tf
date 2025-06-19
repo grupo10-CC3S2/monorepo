423cb35e2954e4fb9c337948154d7c1ef9f68aad
Alex Vega <axvg@users.noreply.github.com>
2025-06-19 02:26:11 -0500

    feat(tf): agrega output para mostrar info resumida

--- modules/terraform/storage_module/outputs.tf
+++ modules/terraform/storage_module/outputs.tf
@@ -5,5 +5,19 @@ output "db_name_out" {
 
 output "storage_path" {
   description = "Path de la db"
-  value = "/db/${var.name}"
+  value       = "/db/${var.name}"
+}
+
+output "storage_info" {
+  description = "Informacion de la db creada"
+  value = {
+    name           = var.name
+    network_name   = var.network_name
+    server_name    = var.server_name
+    size           = var.size
+    backup_enabled = var.backup_enabled
+    region         = var.region
+    version        = "v0.4.0"
+  }
+  depends_on = [var.network_name, var.server_name]
 }
